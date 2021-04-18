586a503dc17d8539fed9e284176946ae00b87764
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2021-04-18 11:11:19 +0200

    Installed the firebase package, and set the connection settings in the config file.

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+const firebaseConfig = {
+    apiKey: "AIzaSyBEvgSljTg6I-EmUBJa8Z8ozGvh81BglDw",
+    authDomain: "posts-30aec.firebaseapp.com",
+    projectId: "posts-30aec",
+    storageBucket: "posts-30aec.appspot.com",
+    messagingSenderId: "943716117650",
+    appId: "1:943716117650:web:203313d0c71fd1a7024a4b"
+  };
+  //init firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //init firestore service
+  const projectPost = firebase.firestore();
+
+  export { projectPost}
